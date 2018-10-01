f7785da59efd09724e01aafd6f66dc15cd7985e1
Oleksandr Skachkov <gskachkov@gmail.com>
2018-10-01 23:06:59 +0300

    Update wasm

--- paint-worklet/ripple/paintworklet.js
+++ paint-worklet/ripple/paintworklet.js
@@ -56,4 +56,12 @@ registerPaint('ripple', class {
     }
 });
 
-onmessage((evt) => console.log(evt));
+function _base64ToArrayBuffer(base64) {
+  var binary_string =  atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array( len );
+  for (var i = 0; i < len; i++)        {
+      bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
